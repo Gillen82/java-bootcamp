1b3c529ec2ad72d89c7a72bc4cb57bccfd254c69
Stevie Gillen <stevie_gillen@hotmail.com>
2026-01-21 21:41:05 +0000

    Workbook 1 and Challenge one completed

---
+++ 01 Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println(" SSS     GGG ");
+         System.out.println("S    S  G    G");
+         System.out.println("S       G");
+         System.out.println(" SSS    G  GGG");
+         System.out.println("    S   G    G");
+         System.out.println("S   S   G    G");
+         System.out.println(" SSS     GGG");
+    }
+}
