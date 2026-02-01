d4a27b99230927c56432a174d481b81a066e8f3a
Stevie Gillen <stevie_gillen@hotmail.com>
2026-02-01 22:10:41 +0000

    Return strings from functions

--- 04 Functions/Practice/ReturnValues.java
+++ 04 Functions/Practice/ReturnValues.java
@@ -6,10 +6,41 @@ public class ReturnValues {
         System.out.println("Area 1: " + area1);
         System.out.println("Area 2: " + area2);
         System.out.println("Area 3: " + area3);
+
+        String englishExplanation = explainArea("English");
+        System.out.println(englishExplanation);
+
+        String frenchExplanation = explainArea("French");
+        System.out.println(frenchExplanation);
+
+        String spanishExplanation = explainArea("Spanish");
+        System.out.println(spanishExplanation);
+
+        String italianExplanation = explainArea("Italian");
+        System.out.println(italianExplanation);
         
     }
-    public static double calculateArea(double width, double height) {
-        double area = width * height;
+    public static double calculateArea(double width, double length) {
+        if (width <= 0 || length <= 0) {
+            System.out.println("Invalid value has been entered!");
+            System.exit(0);
+        }
+        double area = width * length;
         return area;
     }
+
+    public static String explainArea(String Language) {
+        switch (Language) {
+            case "English": return "Area equals length * width"; 
+            case "French": return "La surface est egale a la longueur * la largeur";
+            case "Spanish": return "area es igual a largo * ancho";
+            default: return "Language not available";
+        }
+    }
 }
+
+// English "Area equals length * width"
+
+// French "La surface est egale a la longueur * la largeur"
+
+// Spanish "area es igual a largo * ancho"
